57344a3453e9e939b670fe0010c98f391dfcff0d
Ulises Gascon <ulises.gascon@guidesmiths.com>
2019-10-01 16:37:49 +0200

    Feature setToken added
    Related #7

--- functions/store.js
+++ functions/store.js
@@ -1,14 +1,20 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
-admin.initializeApp(functions.config().firebase)
 
+admin.initializeApp(functions.config().firebase)
+const tokenRef = 'secrets/badgr'
+
+async function setToken (data) {
+  if (!data || !data.access_token) throw (new Error('No token provided!'))
+  return admin.database().ref(tokenRef).set(data)
+}
 async function getToken () {
-  const snapshot = await admin.database().ref('secrets/badgr').once('child_added')
+  const snapshot = await admin.database().ref(tokenRef).once('child_added')
   const data = snapshot.val()
   if (!data) throw (new Error('No token stored!'))
   return data
 }
 
 module.exports = {
-  getToken
+  getToken, setToken
 }
