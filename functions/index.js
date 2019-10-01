9c10dec959dfd31d4e7c08753392b6ad0b284fc2
Ulises Gascon <ulises.gascon@guidesmiths.com>
2019-10-01 11:18:28 +0200

    Improved Cloud functions Env.
    - See: Fix #4 
    - Added Basic Jest testing
    -Added Husky for Git Hooks
    - Added Standard for linting
    - Improved NPM Scripts

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
+  response.send('Hello from Firebase!')
+})
