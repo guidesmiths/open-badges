35ec5f58a390068f5403c8b3466ff8486be5a399
Ulises Gascon <ulises.gascon@guidesmiths.com>
2019-10-02 10:40:52 +0200

    linter

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,12 @@ const { setToken } = require('./store')
 const { generateToken } = require('./badgr')
 
 exports.dailyCrontab = functions.region('us-central1').pubsub.schedule('* 6 * * *').onRun(async (context) => {
-    console.log('[TOKEN] Refresh has started');
-    try {
-        const token = await generateToken();
-        await setToken(token);
-        console.log('[TOKEN] Refresh has been updated');
-    } catch(err) {
-        console.error('[TOKEN] ERROR!', err);
-    }
-});
+  console.log('[TOKEN] Refresh has started')
+  try {
+    const token = await generateToken()
+    await setToken(token)
+    console.log('[TOKEN] Refresh has been updated')
+  } catch (err) {
+    console.error('[TOKEN] ERROR!', err)
+  }
+})
