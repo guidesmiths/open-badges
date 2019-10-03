8887094a0be8b5665a11fa490da5a0f99ee7c305
Ulises Gascon <ulises.gascon@guidesmiths.com>
2019-10-03 14:05:14 +0200

    Feature: getBadgesList added
    - Related #6
    - Added `getBadgesList`
    - Minor refactor for badgr api tests
    - Mocks improved for reusability

--- functions/__tests__/badgr.test.js
+++ functions/__tests__/badgr.test.js
@@ -1,22 +1,29 @@
-const { generateToken } = require('../badgr')
-const nock = require('nock')
+const { generateToken, getBadgesList } = require('../badgr')
+const { mockToken, mockBadgeClasses } = require('../__samples__/index')
+const { mockTokenRequest, mockBadgeClassesRequest } = require('../__helpers__/index')
+const admin = require('firebase-admin')
+
+afterEach(() => {
+  admin.flush()
+  jest.clearAllMocks()
+})
 
 describe('Generate Token', () => {
   test('It should generate a valid token', async () => {
-    const response = {
-      access_token: 'acc3ss_t0k3n',
-      token_type: 'Bearer',
-      expires_in: 86400,
-      refresh_token: 'r3fr3sh_t0k3n',
-      scope: 'rw:profile rw:issuer rw:backpack'
-    }
-
-    nock('https://api.badgr.io/o/token')
-      .post('/')
-      .query({ username: 'demo-user', password: 'demo-pass' })
-      .reply(200, JSON.stringify(response))
-
+    mockTokenRequest()
+    const response = mockToken()
     const token = await generateToken()
     expect(token).toStrictEqual(response)
+  })
+})
+
+describe('handle API Rest', () => {
+  test('It should return a full badges list from an issuer', async () => {
+    admin.setDatabase({ 'secrets/badgr': mockToken() })
+    mockBadgeClassesRequest()
+
+    const response = mockBadgeClasses()
+    const list = await getBadgesList()
+    expect(list).toStrictEqual(response.result)
   })
 })
