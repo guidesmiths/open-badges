da6a4c77f1043eec1e2b981a670cebacd8d87147
Ulises Gascon <ulises.gascon@guidesmiths.com>
2019-10-04 09:14:06 +0200

    Feature: badgesDigestor added
    Fix #14

--- functions/__tests__/utils/index.test.js
+++ functions/__tests__/utils/index.test.js
@@ -1,4 +1,26 @@
-const { hashEmail } = require('../../utils/index')
+
+const admin = require('firebase-admin')
+const { hashEmail, badgesDigestor } = require('../../utils/index')
+const { mockBadgeClasses, mockToken, mockBadgeClassesAsserts } = require('../../__samples__/index')
+const { mockBadgeClassesRequest, mockBadgeClassesAssertsRequest } = require('../../__helpers__/index')
+
+function addToken () {
+  admin.setDatabase({ 'secrets/badgr': mockToken() })
+}
+
+function mockDigestorHttpRequest () {
+  mockBadgeClassesRequest()
+
+  mockBadgeClasses().result.forEach(badge => {
+    mockBadgeClassesAssertsRequest(badge.entityId)
+  })
+}
+
+afterEach(() => {
+  admin.flush()
+  jest.clearAllMocks()
+})
+
 describe('Email hashing', () => {
   test('Should generate a valid hash', () => {
     expect(hashEmail('demo@demo.com')).toStrictEqual('53444f91e698c0c7caa2dbc3bdbf93fc')
@@ -13,5 +35,36 @@ describe('Email hashing', () => {
   test('Should throw an error if the email wasn\'t provided', () => {
     expect(() => { hashEmail() }).toThrow('No valid email')
     expect(() => { hashEmail('') }).toThrowError('No valid email')
+  })
+})
+
+describe('badges digestor behaviour', () => {
+  test('Should perform all the HTTP requests expected', async () => {
+    addToken()
+    mockDigestorHttpRequest()
+
+    const badgesList = mockBadgeClasses().result
+    await badgesDigestor(badgesList)
+  })
+
+  test('Should perform all the changes in database expected', async () => {
+    addToken()
+    mockDigestorHttpRequest()
+
+    const badgesList = mockBadgeClasses().result
+    await badgesDigestor(badgesList)
+
+    const uniqueItemsCount = mockBadgeClassesAsserts().result.length
+    const saveAllBadgesCount = 1
+    const totalItems = uniqueItemsCount + saveAllBadgesCount
+
+    expect(admin.set.mock.calls.length).toBe(totalItems)
+  })
+
+  test('Should return an error if there is no list', async () => {
+    expect(badgesDigestor()).rejects.toThrowError('No list provided!')
+    expect(admin.database.mock.calls.length).toBe(0)
+    expect(admin.ref.mock.calls.length).toBe(0)
+    expect(admin.set.mock.calls.length).toBe(0)
   })
 })
--- functions/utils/index.js
+++ functions/utils/index.js
@@ -1,5 +1,15 @@
 const crypto = require('crypto')
 const isEmail = require('email-validator')
+const { saveAllBadges, saveUserBadges } = require('../store')
+const { getBadgeAssertions } = require('../badgr')
+
+function badgesListTransformation (list) {
+  const badges = {}
+  list.forEach(badge => {
+    badges[badge.entityId] = badges
+  })
+  return badges
+}
 
 function generateMd5Hash (txt) {
   return crypto.createHash('md5').update(txt).digest('hex')
@@ -12,4 +22,34 @@ function hashEmail (email) {
   return generateMd5Hash(email)
 }
 
-module.exports = { hashEmail }
+async function badgesDigestor (list) {
+  if (!Array.isArray(list)) throw (new Error('No list provided!'))
+  const userBadges = {}
+  const badgesList = badgesListTransformation(list)
+
+  await saveAllBadges(list)
+
+  const badgesIdsList = list.map(badge => badge.entityId)
+  const badgesAssetsDetails = await Promise.all(badgesIdsList.map(getBadgeAssertions))
+
+  badgesAssetsDetails.forEach(badge => {
+    badge.forEach(assertion => {
+      const email = assertion.recipient.plaintextIdentity
+      const userId = hashEmail(email)
+      const { entityId, issuedOn, evidence, revoked, revocationReason } = assertion
+
+      if (!userBadges[userId]) userBadges[userId] = []
+
+      const badgeData = Object.assign({}, badgesList[entityId], { issuedOn, evidence, revoked, revocationReason })
+
+      const hasBadge = userBadges[userId].filter(badgeStore => badgeStore.entityId === badgeData.entityId)
+      if (hasBadge.length > 0) {
+        userBadges[userId].push(badgeData)
+      }
+    })
+  })
+
+  await Promise.all(Object.keys(userBadges).map(userId => saveUserBadges(userId, userBadges[userId])))
+}
+
+module.exports = { hashEmail, badgesDigestor }
