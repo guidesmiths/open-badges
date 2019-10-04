4dd23f2a20d09fe726b51164add47a1dcb07b2a8
Ulises Gascon <ulises.gascon@guidesmiths.com>
2019-10-04 11:00:40 +0200

    Added Hourly cron job
    - Fix #8
    - Badge digestor is running now every hour
    - Database sync
    - HTTP requests in blocks

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions')
 const { setToken } = require('./store')
-const { generateToken } = require('./badgr')
+const { generateToken, getBadgesList } = require('./badgr')
+const { badgesDigestor } = require('./utils/index')
 
 exports.dailyCrontab = functions.region('us-central1').pubsub.schedule('* 6 * * *').onRun(async (context) => {
   console.log('[TOKEN] Refresh has started')
@@ -10,5 +11,16 @@ exports.dailyCrontab = functions.region('us-central1').pubsub.schedule('* 6 * * 
     console.log('[TOKEN] Refresh has been updated')
   } catch (err) {
     console.error('[TOKEN] ERROR!', err)
+  }
+})
+
+exports.hourlyCrontab = functions.region('us-central1').pubsub.schedule('0 * * * *').onRun(async (context) => {
+  console.log('[Badges] collection has started')
+  try {
+    const badgesList = await getBadgesList()
+    await badgesDigestor(badgesList)
+    console.log('[Badges] collection has been updated')
+  } catch (err) {
+    console.error('[Badges] collection ERROR!', err)
   }
 })
